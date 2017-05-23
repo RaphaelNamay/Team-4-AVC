c9fdc44318afbc215b737fee8a522de6e1d244fa
dowdendani <djd543@gmail.com>
2017-05-23 15:49:45 +1200

    Update pid3-1.cpp

--- pid3-1.cpp
+++ pid3-1.cpp
@@ -24,7 +24,7 @@ void move(int left, int right){
 } 
 
 void moveSt(int leftSt, int rightSt){
-	set_motor(1,(int)right);   
+    set_motor(1,(int)right);   
     set_motor(2,(int)left);
     sleep1(1,0);
 }
@@ -67,13 +67,13 @@ int cameraRead(){
 	
 	//Error control
 	float  error;
-        float current_error =0;
+    float current_error =0;
 	float previous_error =0;
 	float  numWhite=0;
-	//int numDark = 0;
  	float  whiteProportion = 0; //proportion of white pixels in image
-	numWhiteLeft = 0;
-	numWhiteRight =0;
+	float numWhiteLeft = 0;
+	float numWhiteRight =0;
+	float numWhiteTop =0;
 	
 	//PID constants
 	float kp = 0.02;
@@ -91,7 +91,7 @@ int cameraRead(){
 	take_picture();
 	 //display_picture(0,5000000);
 				
-				for(int i=0; i<240; i++){				     //for left	
+				for(int i=0; i<240; i++){				     //for left	verticle
 					p = get_pixel(i,80,3);						
 					if(p>whiteThreshold){   
 						s = 1;
@@ -99,23 +99,29 @@ int cameraRead(){
 					}
 				}					
 							
-				for(int i=0; i<240; i++){					//for Right
+				for(int i=0; i<240; i++){					//for Right verticle
 					p = get_pixel(i,240,3);							
 					if(p>whiteThreshold){   
 						s = 1;
 						numWhiteRight++;
 					}	
 				}	 	 
-							
-				for(int i=0; i<320; i++){					 					
+				
+				for(int i=0; i<320; i++){					//for middle horizontal 					
+					p = get_pixel(210,i,3);			
+					if(p>whiteThreshold){   
+						s = 1;
+						numWhiteTop++;												
+					}
+						
+				for(int i=0; i<320; i++){					//for middle horizontal 					
 					p = get_pixel(120,i,3);			
 					if(p>whiteThreshold){   
 						s = 1;
 						numWhite++;												
-					}else{
-						s = 0;
-						//numDark++;
-							
+					}
+					else{
+						s = 0;												
 					}
 					//printf("%d\n", s); 
 					error = (i-160)*s; // if white is on left, error < 0, if on right, error > 0, if in center, error = 0
@@ -126,19 +132,15 @@ int cameraRead(){
 				printf("number of white %.2f \n",numWhite);
                 printf("current error %.2f \n",current_error);
 				whiteProportion = (numWhite/320.0)*100;
-				if(whiteProportion >=80 && numWhiteLeft >=30 && numWhiteRight >=30){ // Subject to change
-					quadrant = 3;
-					//intersection ++;
-				}
-				sleep1(0,10000);				
-				//Integral				
-				//integral_signal = total_error*ki;
-				//printf("Integral signal is: %d", integral_signal );
-				//printf("current error is: %d", current_error);
 				
+				if(whiteProportion >=80 && numWhiteLeft >=30 && numWhiteRight >=30){ // Subject to change
+				quadrant = 3;					
+				}				
+				sleep1(0,10000);
+														
 				//Proportional
 				proportional_signal = current_error*kp;  
-//				printf("Proportional signal is: %d \n", proportional_signal ); 
+ 
 				
 				//Derivative
 				derivative_signal = (current_error-previous_error/0.01)*kd;
@@ -147,13 +149,13 @@ int cameraRead(){
 				
 				//final signal
 				//final_signal = (((proportional_signal/(160*1*kp)) + (derivative_signal/((320/0.1)*1*kd)))/2)*255;
-				//	final_signal = (proportional_signal/160)*60;
 				final_signal = ((proportional_signal+derivative_signal)/160)*45;
 				printf("final signal is: %.2f \n", final_signal);
 				if(numWhite>0){
 					left = 30 - (final_signal);
 					right = 30 +(final_signal);
-                }else{
+                }
+                else{
 					printf("---------------------------------------------------------------------");
 					left=-25;
 					right=-25;
@@ -161,10 +163,7 @@ int cameraRead(){
                 }
 				printf("left is: %.2f \n",left);
 				printf("right is: %.2f \n", right);
-				//	printf("num whit is:  %d \n", numWhite);
-				//	printf("Proportion:  %d \n", whiteProportion);
-				//current_error = 160-whiteMid;// how far away the center of the white line is from the center of the camera
-							
+						
 return 0;  
 }
 
@@ -178,61 +177,53 @@ int main(){
 			}	
 			
 			while(quadrant == 3){
-						
-						if (numWhiteLeft >=30 && numWhiteRight>=30){           //detects T intersection
+		    cameraRead();		
+						if (whiteProportion >=80 && numWhiteLeft >=30 && numWhiteRight >=30){           //detects T intersection
 							
-							if(intersection == 0){ //go straight on
-								cameraRead();			
+							if(intersection == 0){ //go straight on											
 						        moveSt(30,30);
 						        sleep1(1,0);
 						        intersection =1;	
 							}
 							if(intersection == 1){ // go left
 								sleep1(0,200000);
-								turnLeft();
-								cameraRead();
-								sleep1(1,0);
-								
-							}		
-						
+								turnLeft();								
+								sleep1(1,0);								
+							}						
 						}
 											
-						if (numWhiteLeft >=30 && numWhiteright<30){           //detects left turn
-							
-							if(leftTurn == 0){ //go straight on
-								cameraRead();			
-						        move(left,right);
-						        leftTurn = 1;									
-							}
-							if(leftTurn == 1){  //go left	
-								sleep1(0,100000);
-								turnLeft();	
-								sleep1(0,100000);						
-							}
+//						if (numWhiteLeft >=30 && numWhiteright<30 && numWhiteTop<30){           //detects left turn
+//							
+//							if(leftTurn == 0){ //go straight on
+//								cameraRead();			
+//						        move(left,right);
+//						        leftTurn = 1;									
+//							}
+//							if(leftTurn == 1){  //go left	
+//								sleep1(0,100000);
+//								turnLeft();	
+//								sleep1(0,100000);						
+//							}
+//						}
+//											
+//						if (numWhiteLeft <30 && numWhiteRight>=30  && numWhiteTop<30){           // detects right turn						
+//							if(rightTurn ==	0){ //turn right every time
+//								sleep1(0,100000);
+//								turnright();
+//								sleep1(0,100000);
+//							}
+//							
+						else{            // straight line		
+						    move(left,right);	
 						}
-											
-						if (numWhiteLeft <30 && numWhiteRight>=30){           // detects right turn						
-							if(rightTurn ==	0){ //turn right every time
-								sleep1(0,100000);
-								turnright();
-								sleep1(0,100000);
-							}
-							
-						if(numWhiteLeft <30 && numWhiteRight <30){            // straight line
-						cameraRead();			
-						move(left,right);	
-						}
-						
-							
-						}						
-								
+				
+					}														
 			}
 			
 			while(quadrant == 4){
 				
 			}
-			
-			
+									
 return 0;
 }
 
